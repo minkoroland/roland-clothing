cf21b98d8d471ed5f344b5069ef2aed73b92673b
Roland <minko.roland@dxc.com>
2021-12-06 15:11:16 +0100

    created project with create-react-app with a homepage

---
+++ roland-clothing/src/App.js
@@ -0,0 +1,12 @@
+import './homepage.styles.scss';
+import HomePage from './homepage.component';
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
