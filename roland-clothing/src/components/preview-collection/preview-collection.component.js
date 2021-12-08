32b40c09e6137637ff6f96b835dd4d698fea6385
Roland <minko.roland@dxc.com>
2021-12-08 16:22:24 +0100

    nice webshop layout with big pictures

---
+++ roland-clothing/src/components/preview-collection/preview-collection.component.js
@@ -0,0 +1,21 @@
+import CollectionItem from "../collection-item/collection-item.component";
+import "./preview-collection.styles.scss";
+
+const CollectionPreview = ({ title, items }) => {
+  return (
+    <div className="collection-preview">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <div className="preview">
+        {
+            items
+            .filter((item, index) => index < 4)
+            .map(({ id, ...otherProps }) => (
+                <CollectionItem key={id} {...otherProps} />
+            ))
+          }
+      </div>
+    </div>
+  );
+};
+
+export default CollectionPreview;
--- roland-clothing/src/pages/shop/shop.component.js
+++ roland-clothing/src/pages/shop/shop.component.js
@@ -1,16 +1,23 @@
 import './shop.styles.scss';
 import React, {Component} from 'react';
 import SHOP_DATA from './shop.data';
+import CollectionPreview from '../../components/preview-collection/preview-collection.component';
 
 class Shop extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            sections: SHOP_DATA
+        }
     }
     render() {
         return (
             <div>
-                {SHOP_DATA.map(({title}) => title)}
+                {
+                    this.state.sections.map(({id, ...otherProps})=>(
+                        <CollectionPreview key={id} {...otherProps} />
+                    ))
+                }
             </div>
         );
     }
