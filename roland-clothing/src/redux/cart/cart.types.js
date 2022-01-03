751d0f94626b490697c8586eab27a09597ab1e78
Roland <minko.roland@dxc.com>
2022-01-03 11:22:08 +0100

    refresh does not clear the cart, which is good for us

--- roland-clothing/src/redux/cart/cart.types.js
+++ roland-clothing/src/redux/cart/cart.types.js
@@ -2,7 +2,7 @@ const CartActionTypes = {
   TOGGLE_CART: "TOGGLE_CART",
   ADD_ITEM: "ADD_ITEM",
   CLEAR_ITEM: "CLEAR_ITEM",
-  REMOVE_ITEM: 'REMOVE_ITEM'
+  REMOVE_ITEM: "REMOVE_ITEM"
 };
 
 export default CartActionTypes;
